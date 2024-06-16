25afe9e873c5f3f9e27518bd0387158f485848fa
Alameda-Indiano <thiago2332003@gmail.com>
2024-06-16 12:41:55 -0300

    Criando Tela Inicial

--- src/front-end/pages/hello/index.js
+++ src/front-end/pages/hello/index.js
@@ -1,13 +1,21 @@
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { Link } from 'react-router-native';
+import imageGraphic from '../../assets/graphic.png';
 import { homePageStyles } from './styleds.js';
-
 
 export const Hello = () => {
   return (
     <View style={homePageStyles.container}>
       <Text style={homePageStyles.title} >
-        Oie, seja bem vindo ao Fitnes App
+        Registro
       </Text>
+      <Image style={homePageStyles.image} source={imageGraphic} />
+      <Text style={homePageStyles.description} >
+        Anotando pesos e <br /> repetições de cada treino, <br /> vai te dar mais oportunidade <br /> e motivação para atingir os seus objetivos!
+      </Text>
+      <Link to="/login" style={homePageStyles.button}>
+        <Text style={homePageStyles.buttonText} >Vá Para Os Treinos</Text>
+      </Link>
     </View>
   );
 }
--- src/front-end/pages/hello/styleds.js
+++ src/front-end/pages/hello/styleds.js
@@ -5,11 +5,34 @@ export const homePageStyles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#2c2c2e',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-around',
   },
   title: {
-    fontSize: '.8rem',
     color: "#fff",
     fontSize: '1.3rem'
+  },
+  description: {
+    fontSize: '.9rem',
+    textAlign: 'center',
+    color: '#c3c3c3'
+  },
+  image: {
+    width: '50%',
+    height: '25%'
+  },
+  button: {
+    backgroundColor: '#0cfb8c',
+    height: '50px',
+    width: '70%',
+    minWidth: '100px',
+    borderRadius: '8px',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontWeight: "bold",
+    fontSize: '1rem'
   },
 });
